4d3a548f50f2b3dfa4f38e468b12ecff28fc66c9
ignaciollado <86431188+ignaciollado@users.noreply.github.com>
2025-06-11 14:10:27 +0200

    genero el componente home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { GrantApplicationFormComponent } from './xecs/grant-application-form/grant-application-form.component';
+import { HomeComponent } from './home.component';
 
 export const routes: Routes = [
-  { path: 'home', component: GrantApplicationFormComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'grant-subsidies-application-form', component: GrantApplicationFormComponent},
   { path: 'contact', component: GrantApplicationFormComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
