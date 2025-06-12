1d438bf0338ee4b88b8bae39889871715087f5ec
ignaciollado <86431188+ignaciollado@users.noreply.github.com>
2025-06-12 08:10:15 +0200

    creo forms isba e ils

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { GrantApplicationFormComponent } from './xecs/grant-application-form/grant-application-form.component';
+import { IsbaGrantApplicationFormComponent } from './isba/grant-application-form.component';
+
 import { HomeComponent } from './home.component';
+import { Component } from '@angular/core';
+import { IlsGrantApplicationFormComponent } from './ils/grant-application-form.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'grant-subsidies-application-form', component: GrantApplicationFormComponent},
+  { path: 'xecs-grant-subsidies-application-form', component: GrantApplicationFormComponent},
+  { path: 'isba-grant-subsidies-application-form', component: IsbaGrantApplicationFormComponent},
+  { path: 'ils-grant-subsidies-application-form', component: IlsGrantApplicationFormComponent},
   { path: 'contact', component: GrantApplicationFormComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', component: GrantApplicationFormComponent }
+  { path: '**', component: HomeComponent }
 ];
