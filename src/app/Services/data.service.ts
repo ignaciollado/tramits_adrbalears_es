fc603c6144f5f58050b3471c38f5d724e732e3cb
JLDJadr <jose.luis@adrbalears.es>
2025-06-13 09:09:29 +0200

    Añado servicio de datos comunes

---
+++ src/app/Services/data.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private urlAPIMock: string;
+  private urlAPIIbrelleu: string;
+
+  constructor(private http: HttpClient) {
+    this.urlAPIMock = '../../assets/json'
+    this.urlAPIIbrelleu = 'https://data.ibrelleu.es/public/index.php'
+  }
+
+  // Zipcode
+  getZipCodes(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.urlAPIIbrelleu}/zipcodes`).pipe(
+      catchError(error => {
+        console.error('Error en getZipCodes(): ', error)
+        return throwError(() => error)
+      })
+    )
+  }
+
+  // IAE
+  getAllMockIAE(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.urlAPIMock}/epigrafeIAE.json`).pipe(
+      catchError(error => {
+        console.error('Error en getAllMockIAE(): ', error)
+        return throwError(() => error)
+      })
+    )
+  }
+}
