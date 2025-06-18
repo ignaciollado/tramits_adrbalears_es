bd37cb239b175077cf0c445f7b5856c54f79584d
JLDJadr <jose.luis@adrbalears.es>
2025-06-18 08:57:37 +0200

    Arreglo problema con validador CIF

--- src/app/Services/custom-validators.service.ts
+++ src/app/Services/custom-validators.service.ts
@@ -36,11 +36,6 @@ export class CustomValidatorsService {
 
   private nieInitialLetters: string[] = ['X', 'Y', 'Z']
 
-  // Patrones para verificar antes de hacer validación si es un nie o si es un DNI
-  // Estos patrones se sacaron en base a la página: https://regexr.com/
-  private niePattern: RegExp = /^[XYZ]\d{7}$/
-  private dniPattern: RegExp = /^\d{8}$/
-
   /* CIF */
   private organizationCode: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'N', 'P', 'Q', 'R', 'S', 'U', 'V', 'W']
 
@@ -64,6 +59,13 @@ export class CustomValidatorsService {
     "I"
   ]
 
+
+  // Patrones para verificar antes de hacer validación si es un NIE, un DNI o un CIF
+  // Estos patrones se sacaron en base a la página: https://regexr.com/
+  private niePattern: RegExp = /^[XYZ]\d{7}$/
+  private dniPattern: RegExp = /^\d{8}$/
+  private cifPattern: RegExp = /^[ABCDEFGHJNPQRSUVW]\d{8}$/
+
   private isSettingValues = new BehaviorSubject<boolean>(false);
 
   cifValidator(): ValidatorFn {
@@ -79,45 +81,47 @@ export class CustomValidatorsService {
       const initialLetter: string = data.charAt(0)
 
       if (data.length == 9 && this.organizationCode.includes(initialLetter)) {
-        let oddList: any[] = [] // Impares
-        let evenList: any[] = [] // Pares
-        const cifNumeration: string[] = data.substring(1, 8).split("")
+        if (this.cifPattern.test(data)) {
+          let oddList: any[] = [] // Impares
+          let evenList: any[] = [] // Pares
+          const cifNumeration: string[] = data.substring(1, 8).split("")
 
-        cifNumeration.forEach((digit, i) => {
-          (i + 1) % 2 === 0 ? evenList.push(digit) : oddList.push(digit)
-        });
+          cifNumeration.forEach((digit, i) => {
+            (i + 1) % 2 === 0 ? evenList.push(digit) : oddList.push(digit)
+          });
 
-        
-        // Cálculo de números pares: Suma de los mismos
-        const sumEven: number = evenList.reduce((a, b) => parseInt(a) + parseInt(b))
-        
-        // Cálculo de números impares: Suma de dígitos tras multiplicarlos * 2 ==> 10 -> 1+0
-        let sumOdd: number = 0;
-        oddList.map(odd => {
-          let oddNumber = parseInt(odd) * 2
-          if (oddNumber > 9) {
-            oddNumber -= 9
+
+          // Cálculo de números pares: Suma de los mismos
+          const sumEven: number = evenList.reduce((a, b) => parseInt(a) + parseInt(b))
+
+          // Cálculo de números impares: Suma de dígitos tras multiplicarlos * 2 ==> 10 -> 1+0
+          let sumOdd: number = 0;
+          oddList.map(odd => {
+            let oddNumber = parseInt(odd) * 2
+            if (oddNumber > 9) {
+              oddNumber -= 9
+            }
+
+            sumOdd += oddNumber
+          })
+
+          // Suma total y cálculo de dígito/letra
+          const totalSum: number = sumEven + sumOdd
+
+          // Si es igual a 10, el valor final es 0
+          const validControlDigit: number = (10 - (totalSum % 10) == 10) ? 0 : (10 - (totalSum % 10))
+
+          if (this.numberControlDigit.includes(initialLetter)) {
+            validData = `${initialLetter}${cifNumeration.join("")}${validControlDigit}`.toString() // Valor numérico
+          } else if (this.letterControlDigit.includes(initialLetter)) {
+            validData = `${initialLetter}${cifNumeration.join("")}${this.controlDigitList[validControlDigit]}`.toString() // Valor alfabético
           }
-
-          sumOdd += oddNumber
-        })
-
-        // Suma total y cálculo de dígito/letra
-        const totalSum: number = sumEven + sumOdd
-
-        // Si es igual a 10, el valor final es 0
-        const validControlDigit: number = (10 - (totalSum % 10) == 10) ? 0 : (10 - (totalSum % 10))
-
-        if (this.numberControlDigit.includes(initialLetter)) {
-          validData = `${initialLetter}${cifNumeration.join("")}${validControlDigit}`.toString() // Valor numérico
-        } else if (this.letterControlDigit.includes(initialLetter)) {
-          validData = `${initialLetter}${cifNumeration.join("")}${this.controlDigitList[validControlDigit]}`.toString() // Valor alfabético
         }
       }
 
       if (validData != null) {
         this.isSettingValues.next(true)
-        control.setValue(validData, { emitEvent: false})
+        control.setValue(validData, { emitEvent: false })
         this.isSettingValues.next(false)
       } else {
         return { cifValidator: false }
