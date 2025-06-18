0b4e9ca2c0f1a40c67126391aa2c98e858aee673
JLDJadr <jose.luis@adrbalears.es>
2025-06-18 08:56:32 +0200

    Formulario ILS terminado

--- src/app/ils/grant-application-form.component.ts
+++ src/app/ils/grant-application-form.component.ts
@@ -34,7 +34,7 @@ export class IlsGrantApplicationFormComponent {
   rgpdAccepted = false
 
   businessType: string = "";
-  businessTypeChoosed: boolean = false
+  businessTypeChoosed: boolean = false // Para evitar problemas de validadores customs
 
   zipCodeList: any[] = []
   filteredOptions: Observable<any[]> | undefined;
@@ -44,6 +44,26 @@ export class IlsGrantApplicationFormComponent {
 
   checkboxID: boolean = true
   checkboxATIB: boolean = true
+
+  fileNames: { [key: string]: string } = {
+    file_enviardocumentoIdentificacion: "",
+    file_certificadoATIB: "",
+    file_escritura_empresa: "",
+    file_certificado_IAE: "",
+    file_informeResumenIls: "",
+    file_informeInventarioIls: "",
+    file_certificado_verificacion_ISO: "",
+    file_modeloEjemploIls: "",
+    file_certificado_itinerario_formativo: "",
+    file_memoriaTecnica: "",
+    file_nifEmpresa: "",
+    file_logotipoEmpresaIls: ""
+  }
+
+  // 10 MB máximos
+  maxFileSizeBytes: number = 10 * 1024 * 1024
+
+  radioOptionDocs: string = ""
 
 
   constructor(private dataService: DataService, private customValidator: CustomValidatorsService, private fb: FormBuilder) {
@@ -63,16 +83,29 @@ export class IlsGrantApplicationFormComponent {
       nif_representante: this.fb.control<string>('', [Validators.required, Validators.minLength(9), Validators.maxLength(9), this.customValidator.dniNieValidator()]),
       tel_representante: this.fb.control<string>('', [Validators.required, Validators.pattern('[0-9]{9}'), Validators.maxLength(9)]),
       mail_representante: this.fb.control<string>('', [Validators.required, Validators.email]),
-      // Nombres temporales
+
       checkboxID: this.fb.control<boolean>(true),
+      file_enviardocumentoIdentificacion: this.fb.control<File | null>(null),
       checkboxATIB: this.fb.control<boolean>(true),
-      file_enviardocumentoIdentificacion: this.fb.control<string>(''),
-      file_certificadoATIB: this.fb.control<string>(''),
-      
+      file_certificadoATIB: this.fb.control<File | null>(null),
+
       declaracion_responsable_i: this.fb.control<boolean>(true),
       declaracion_responsable_v: this.fb.control<boolean>(true),
       declaracion_responsable_vii: this.fb.control<boolean>(true),
-      declaracion_responsable_ix: this.fb.control<boolean>(true)
+      declaracion_responsable_ix: this.fb.control<boolean>(true),
+
+      // Documentación
+      file_escritura_empresa: this.fb.control<File | null>(null, [Validators.required]),
+      file_certificado_IAE: this.fb.control<File | null>(null, [Validators.required]),
+      file_informeResumenIls: this.fb.control<File | null>(null), // Primera opción radio
+      file_informeInventarioIls: this.fb.control<File | null>(null), // Primera opción radio
+      file_certificado_verificacion_ISO: this.fb.control<File | null>(null), // Segunda opción radio
+
+      file_modeloEjemploIls: this.fb.control<File | null>(null, [Validators.required]),
+      file_certificado_itinerario_formativo: this.fb.control<File | null>(null, [Validators.required]),
+      file_memoriaTecnica: this.fb.control<File | null>(null),
+      file_nifEmpresa: this.fb.control<File | null>(null),
+      file_logotipoEmpresaIls: this.fb.control<File | null>(null)
 
     })
 
@@ -105,13 +138,13 @@ export class IlsGrantApplicationFormComponent {
       })
     );
 
-    // Pendiente de input files.
+    // Lógica para añadir/quitar validator required en docs autorizaciones
     this.ilsForm.get('checkboxID')?.valueChanges.subscribe((value: boolean) => {
-      this.checkboxID = value
-    })
+      this.applyConditionalValidator("checkboxID", "file_enviardocumentoIdentificacion", value)
+    });
 
     this.ilsForm.get('checkboxATIB')?.valueChanges.subscribe((value: boolean) => {
-      this.checkboxATIB = value
+      this.applyConditionalValidator('checkboxATIB', 'file_certificadoATIB', value)
     })
   }
 
@@ -119,10 +152,63 @@ export class IlsGrantApplicationFormComponent {
     console.log(this.ilsForm.value)
   }
 
+  // Subida individual de archivo.
+  onFileChange(event: Event, controlName: string): void {
+    const file = (event.target as HTMLInputElement).files?.[0]
+    const controlNameForm = this.ilsForm.get(controlName)
+
+    if (file) {
+      controlNameForm?.setValue(file)
+      if (file.size > this.maxFileSizeBytes) {
+        controlNameForm?.setErrors({ invalidFile: true })
+      }
+      else {
+        controlNameForm?.setErrors(null)
+      }
+      this.fileNames[controlName] = file.name
+    }
+  }
+
+  // Validadores dinámicos y borrado de datos tras seleccionar en el radio de docs.
+  onOptionSelected(option: string): void {
+    const isOption1 = option === 'option1';
+  
+    const option1Fields = ['file_informeResumenIls', 'file_informeInventarioIls'];
+    const option2Fields = ['file_certificado_verificacion_ISO'];
+  
+    option1Fields.forEach(field => {
+      this.ilsForm.get(field)?.setValidators(isOption1 ? Validators.required : null);
+      this.ilsForm.get(field)?.setValue(isOption1 ? this.ilsForm.get(field)?.value : null);
+      this.ilsForm.get(field)?.updateValueAndValidity();
+    });
+  
+    option2Fields.forEach(field => {
+      this.ilsForm.get(field)?.setValidators(isOption1 ? null : Validators.required);
+      this.ilsForm.get(field)?.setValue(isOption1 ? null : this.ilsForm.get(field)?.value);
+      this.ilsForm.get(field)?.updateValueAndValidity();
+    });
+  
+    this.radioOptionDocs = option;
+  }
+
+  private applyConditionalValidator(checkboxName: string, fileControlName: string, checked: boolean): void {
+    const fileControl = this.ilsForm.get(fileControlName)
+
+    if (checked === false) {
+      fileControl?.setValidators([Validators.required])
+    } else {
+      fileControl?.clearValidators();
+      fileControl?.setValue(null)
+    }
+
+    fileControl?.updateValueAndValidity();
+  }
+
   setStep(index: number) {
     this.step.set(index)
   }
 
+  // Cambio de custom-validator según tipo solicitante
   changeNIFValidator(): void {
     const nifControl = this.ilsForm.get('nif')
     const tipo_solicitanteValue = this.ilsForm.get('tipo_solicitante')?.value
@@ -140,6 +226,7 @@ export class IlsGrantApplicationFormComponent {
     nifControl?.setValidators(nifValidators)
     nifControl?.updateValueAndValidity()
     this.businessTypeChoosed = true
+    this.setStep(2);
   }
 
   // Limpieza espacios en blanco
@@ -160,5 +247,6 @@ export class IlsGrantApplicationFormComponent {
     const filterValue = name;
     return this.options.filter((option) => option.zipCode.includes(filterValue))
   }
+
 
 }
