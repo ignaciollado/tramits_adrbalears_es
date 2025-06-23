57bbf33e134d4cba8dc80e3e9343e8680645ba0e
JLDJadr <jose.luis@adrbalears.es>
2025-06-23 09:35:41 +0200

    Servicio para 'Expedientes'

---
+++ src/app/Services/expediente.service.ts
@@ -0,0 +1,65 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExpedienteService {
+  urlAPITramits: string;
+
+  constructor(private http: HttpClient) {
+    this.urlAPITramits = "https://pre-tramits.idi.es/public/index.php"
+  }
+
+  /* CRUD Expedientes */
+
+  // GET all
+  getAllExpedientes(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.urlAPITramits}/pindustexpediente`).pipe(catchError(this.handleError))
+  }
+
+  // GET by ID
+  getOneExpediente(id: number): Observable<any> {
+    return this.http.get<any[]>(`${this.urlAPITramits}/pindustexpediente/${id}`).pipe(catchError(this.handleError))
+  }
+
+  // GET by Convocatoria
+  getExpedientesByConvocatoria(convocatoria: number): Observable<any> {
+    return this.http.get<any[]>(`${this.urlAPITramits}/pindustexpediente/convocatoria/${convocatoria}`).pipe(catchError(this.handleError))
+  }
+
+  // GET by Convocatoria y tipo trámite
+  getExpedientesByConvocatoriaAndTipoTrámite(convocatoria: number, tipo_tramite?: string): Observable<any> {
+    return this.http.get<any[]>(`${this.urlAPITramits}/pindustexpediente/convocatoria/${convocatoria}?${tipo_tramite}`).pipe(catchError(this.handleError))
+  }
+
+  // POST
+  createExpediente(expediente: any): Observable<any> {
+    return this.http.post<any>(`${this.urlAPITramits}/pindustexpediente/create`, expediente).pipe(catchError(this.handleError))
+  }
+
+  // PUT
+  updateExpediente(id: number, expediente: any): Observable<any> {
+    return this.http.put<any>(`${this.urlAPITramits}/pindustexpediente/update/${id}`, expediente).pipe(catchError(this.handleError))
+  }
+
+  // DELETE
+  deleteExpediente(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.urlAPITramits}/pindustexpediente/delete/${id}`).pipe(catchError(this.handleError))
+  }
+
+
+
+  private handleError(error: HttpErrorResponse) {
+    let errorMessage = 'Unknown error!';
+    if (error.error instanceof ErrorEvent) {
+      // Error del lado del cliente
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // Error del lado del servidor
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(errorMessage);
+  }
+}
