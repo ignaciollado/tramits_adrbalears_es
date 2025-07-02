e738bc3215d67cffeeb94748a81a21aa5dbc9778
ignaciollado <86431188+ignaciollado@users.noreply.github.com>
2025-07-02 11:55:54 +0200

    sigo con la solicitud de xecs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,23 @@
 import { Routes } from '@angular/router';
 import { GrantApplicationFormComponent } from './xecs/grant-application-form/grant-application-form.component';
 import { IsbaGrantApplicationFormComponent } from './isba/grant-application-form/grant-application-form.component';
-
 import { HomeComponent } from './home.component';
-import { Component } from '@angular/core';
 import { IlsGrantApplicationFormComponent } from './ils/grant-application-form/grant-application-form.component';
-
+import { LoginComponent } from './login/login.component';
+import { authGuard } from './auth/auth.guard';
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent  },
   { path: 'xecs-grant-subsidies-application-form', component: GrantApplicationFormComponent},
   { path: 'isba-grant-subsidies-application-form', component: IsbaGrantApplicationFormComponent},
   { path: 'ils-grant-subsidies-application-form', component: IlsGrantApplicationFormComponent},
+
+
+  { path: 'xecs-management', canActivate: [authGuard], component:HomeComponent },
+  { path: 'isba-management', canActivate: [authGuard], component:HomeComponent },
+  { path: 'ils-management', canActivate: [authGuard], component:HomeComponent },
+
   { path: 'contact', component: GrantApplicationFormComponent },
   { path: '*', redirectTo: 'home', pathMatch: 'full' },
   { path: '**', component: HomeComponent }
