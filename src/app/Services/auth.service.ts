2608fb521628daa68ebd92aef42cf18b559cad25
ignaciollado <86431188+ignaciollado@users.noreply.github.com>
2025-07-03 07:15:00 +0200

    sigo con la solicitud de xecs

---
+++ src/app/Services/auth.service.ts
@@ -0,0 +1,122 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, map, Observable, throwError } from 'rxjs';
+import { AuthDTO } from '../Models/auth.dto';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { PasswordGeneratorService } from './password-generator-service';
+import { BehaviorSubject } from 'rxjs';
+
+export interface AuthToken {
+  user_id: string;
+  access_token: string;
+  days_to_expire_pwd: string;
+}
+
+const URL_API = '../../assets/phpAPI/'
+const URL_API_SRV = "https://jwt.idi.es/public/index.php"
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'text/plain', /* la única forma de evitar errores de CORS ha sido añadiendo esta cabecera */
+  })
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+ 
+  constructor(
+    private http: HttpClient, 
+    private jwtHelper: JwtHelperService,
+    private passwordService: PasswordGeneratorService) { }
+
+  public login(loginForm: AuthDTO): Observable<AuthToken> {
+    return this.http
+      .post<AuthToken>( `${URL_API_SRV}/api/login-users/`, loginForm, httpOptions )
+  }
+
+  public logout(): void {
+    sessionStorage.removeItem('access_token');
+    sessionStorage.removeItem('ibrelleu_user');
+    window.location.reload()
+  }
+
+  public isAuthenticated(): boolean {
+    if (this.jwtHelper.isTokenExpired(sessionStorage.getItem("access_token"))) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  createUser(userData: any): Observable<any> {
+    userData.name = userData.aodl
+    userData.password = this.passwordService.generatePassword()
+    userData.id_ils = 284
+    userData.role = userData.profile
+    return this.http.post(`${URL_API_SRV}/create-user-ibrelleu`, userData)
+    .pipe(
+      
+    map((response: any) => {
+      return response;
+    }),
+ catchError((error: HttpErrorResponse) => {
+ let errorMsg = 'Error desconocido al crear el usuario.';
+ if (error.error?.message) {
+ errorMsg = error.error.message;
+ } else if (error.status === 0) {
+ errorMsg = 'No se pudo conectar con el servidor.';
+ } else if (error.status === 400) {
+ errorMsg = 'Datos inválidos enviados al servidor.';
+ } else if (error.status === 409) {
+ errorMsg = 'El usuario ya existe.';
+ }
+ return throwError(() => new Error(errorMsg));
+ })
+
+    )
+  }
+
+  regeneratePasswordUserApp(userId: number, userData: any): Observable<any> {
+  const password = this.passwordService.generatePassword();
+
+  const payload = {
+    ...userData,
+    name: userData.aodl,
+    password
+  };
+
+  return this.http.post(`${URL_API_SRV}/generate-user-pwd-ibrelleu/${userId}`, payload);
+  }
+
+
+  private userNameSubject = new BehaviorSubject<string>(null);
+  userName$ = this.userNameSubject.asObservable();
+
+  private userRoleSubject = new BehaviorSubject<string>(null);
+  userRole$ = this.userRoleSubject.asObservable();
+
+  private daysToExpirePwdSubject = new BehaviorSubject<number>(null);
+  daysToExpirePwd$ = this.daysToExpirePwdSubject.asObservable();
+
+  setUserInfo(name: string, role: string, daysToExpirePwd: number) {
+    this.userNameSubject.next(name)
+    this.userRoleSubject.next(role)
+    this.daysToExpirePwdSubject.next(daysToExpirePwd)
+  }
+
+/*   setDaysToExpirePwd(days: number) {
+    this.daysToExpirePwdSubject.next(days);
+  } */
+
+  loadFromSession() {
+    const token = sessionStorage.getItem('access_token');
+    if (token) {
+      const decoded = new JwtHelperService().decodeToken(token);
+      const days = +sessionStorage.getItem('days_to_expire_pwd');
+      this.setUserInfo(decoded.name, decoded.role, days);
+      /* this.setDaysToExpirePwd(days); */
+    }
+  }
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,12 +5,17 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { AuthService, AuthToken } from '../Services/auth.service';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    CommonModule,
+    CommonModule, TranslateModule,
     ReactiveFormsModule,
     MatCardModule,
     MatFormFieldModule,
@@ -22,8 +27,14 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  isSuperUser: boolean = false
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private translate: TranslateService,
+        private authService: AuthService, 
+        private router: Router,  
+        private snackBar: MatSnackBar,
+        private jwtHelper: JwtHelperService
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
@@ -34,7 +45,46 @@ export class LoginComponent {
     if (this.loginForm.valid) {
       const { email, password } = this.loginForm.value;
       console.log('Login con:', email, password);
-      // Aquí puedes llamar a un servicio de autenticación
     }
+    let responseOK: boolean = false
+    let errorResponse: any
+    sessionStorage.removeItem('access_token');
+    sessionStorage.removeItem('ibrelleu_user');
+
+    if ( this.loginForm ) {
+        this.authService.login( this.loginForm.value )
+        .subscribe (
+          (item:AuthToken ) => {
+            console.log ("Welcome to IBRelleu Market Place created by the ADR Balears ...")
+            responseOK = true
+            errorResponse = "Logged in"
+            sessionStorage.setItem('ibrelleu_user', this.jwtHelper.decodeToken(item.access_token).name)
+            sessionStorage.setItem('access_token', item.access_token)
+            sessionStorage.setItem("preferredLang", "cat")
+            sessionStorage.setItem("days_to_expire_pwd", item.days_to_expire_pwd)
+            this.authService.setUserInfo(this.jwtHelper.decodeToken(item.access_token).name, this.jwtHelper.decodeToken(item.access_token).role, +item.days_to_expire_pwd);
+
+            this.showSnackBar(errorResponse + " as " + this.jwtHelper.decodeToken(item.access_token).name)
+            if (this.jwtHelper.decodeToken().role === 'admin') {
+              this.isSuperUser = true
+            }
+            this.router.navigate(['/body'])
+            },
+            (error: any) => {
+                  responseOK = false
+                  this.showSnackBar(error)
+                  this.loginForm.reset()
+                },
+                  () => {
+                    console.log("Login complete, redirecting ...")
+                    this.router.navigateByUrl('home')
+                  }
+        )
+    }
+  }
+
+  private showSnackBar(error: string): void {
+    this.snackBar.open( error, 'X', { duration: 10000, verticalPosition: 'top', 
+      horizontalPosition: 'center', panelClass: ["custom-snackbar"]} );
   }
 }
