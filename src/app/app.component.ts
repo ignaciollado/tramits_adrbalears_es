f86ce24ad46034f6e9e77e6960a31598f172c629
ignacio.llado@idi.es <ignacio.llado@idi.es>
2025-07-05 09:53:35 +0200

    sigo con el management de xecs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AuthService } from './Services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  //imports: [RouterLink, RouterOutlet],
-  imports: [RouterModule,  HeaderComponent, FooterComponent],
+  imports: [RouterModule,  HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  
+   constructor(public authService: AuthService) {}
+  logout(): void {
+    this.authService.logout();
+  }
 }
