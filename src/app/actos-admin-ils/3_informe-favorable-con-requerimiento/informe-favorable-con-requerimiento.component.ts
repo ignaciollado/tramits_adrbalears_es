faa770357cc7f84b57dece457541909137376be6
JLDJadr <jose.luis@adrbalears.es>
2025-11-27 10:55:37 +0100

    Informe favorable con requerimiento revisado y corregido para ILS

--- src/app/actos-admin-ils/3_informe-favorable-con-requerimiento/informe-favorable-con-requerimiento.component.ts
+++ src/app/actos-admin-ils/3_informe-favorable-con-requerimiento/informe-favorable-con-requerimiento.component.ts
@@ -20,6 +20,7 @@ import { ExpedienteService } from '../../Services/expediente.service';
 import { PindustLineaAyudaService } from '../../Services/linea-ayuda.service';
 import { PindustConfiguracionService } from '../../Services/pindust-configuracion.service';
 import { ViafirmaService } from '../../Services/viafirma.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-informe-favorable-con-requerimiento-ils',
@@ -35,8 +36,6 @@ export class InformeFavorableConRequerimientoIlsComponent {
   signatureDocState: string = "";
   nifDocGenerado: string = "";
   timeStampDocGenerado: string = "";
-  userLoginEmail: string = "";
-  ceoEmail: string = "";
   pdfUrl: SafeResourceUrl | null = null;
   showPdfViewer: boolean = false;
   nameDocGenerado: string = "";
@@ -68,7 +67,10 @@ export class InformeFavorableConRequerimientoIlsComponent {
   signedBy!: string;
 
   docDataString!: ActoAdministrativoDTO;
-  emailConseller!: string;
+
+  technicianEmail!: string;
+  ceoEmail!: string;
+  consellerEmail!: string;
 
   @Input() actualID!: number;
   @Input() actualIdExp!: number;
@@ -85,7 +87,7 @@ export class InformeFavorableConRequerimientoIlsComponent {
     private lineaAyuda: PindustLineaAyudaService,
     private configGlobal: PindustConfiguracionService
   ) {
-    this.userLoginEmail = sessionStorage.getItem('tramits_user_email') || '';
+    this.technicianEmail = sessionStorage.getItem('tramits_user_email') || '';
   }
 
   get stateClass(): string {
@@ -106,7 +108,7 @@ export class InformeFavorableConRequerimientoIlsComponent {
       })
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
+  ngOnChanges(): void {
     if (this.tieneTodosLosValores()) {
       this.getActoAdminDetail();
       this.getLineDetail(this.actualConvocatoria);
@@ -373,13 +375,10 @@ export class InformeFavorableConRequerimientoIlsComponent {
   }
 
   viewActoAdmin(nif: string, folder: string, filename: string, extension: string): void {
-    const entorno = sessionStorage.getItem('entorno');
+    const entorno = environment.apiUrl;
     filename = filename.replace(/^doc_/, "");
     filename = `${this.actualIdExp}_${this.actualConvocatoria}_${filename}`;
-    let url = "";
-    url = entorno === "tramits" ?
-      `https://tramits.idi.es/public/index.php/documents/view/${nif}/${folder}/${filename}` :
-      `https://pre-tramits.idi.es/public/index.php/documents/view/${nif}/${folder}/${filename}`;
+    const url = `${entorno}/documents/view/${nif}/${folder}/${filename}`;
 
     const sanitizedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
 
@@ -421,17 +420,19 @@ export class InformeFavorableConRequerimientoIlsComponent {
 
     switch (this.signedBy) {
       case 'technician':
-        email = this.userLoginEmail;
+        email = this.technicianEmail;
         break;
       case 'ceo':
         email = this.ceoEmail;
         break;
-      case 'applicant':
-        email = this.form.get('email_rep')?.value;
-        break;
+
       case 'conseller':
         // ToDo
-        email = this.emailConseller;
+        email = this.consellerEmail;
+        break;
+
+      case 'applicant':
+        email = this.form.get('email_rep')?.value;
         break;
     }
 
@@ -440,7 +441,7 @@ export class InformeFavorableConRequerimientoIlsComponent {
       nombreDocumento: filename,
       nif: nif,
       last_insert_id: this.lastInsertId
-    }
+    };
 
     this.viafirmaService.createSignatureRequest(payload)
       .pipe(finalize(() => { this.loading = false; }))
@@ -473,10 +474,13 @@ export class InformeFavorableConRequerimientoIlsComponent {
   }
 
   getGlobalConfig() {
-    this.configGlobal.getActive().subscribe((globalConfigArr: ConfigurationModelDTO[]) => {
-      const globalConfig = globalConfigArr[0];
-        // this.emailConseller = globalConfig.eMailPresidente || ''
-        this.emailConseller = ''
+    this.configGlobal.getActive().subscribe((globalConfig: ConfigurationModelDTO[]) => {
+      /* Quitar hardcodeo de emails */
+      // this.ceoEmail = globalConfig[0].eMailDGerente;
+      // this.consellerEmail = globalConfig[0].eMailPresidente;
+
+      this.ceoEmail = 'jose.luis@idi.es'
+      this.consellerEmail = 'jldejesus@adrbalears.caib.es'
     })
   }
 
